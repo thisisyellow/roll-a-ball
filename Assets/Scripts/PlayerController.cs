31422fd7a5593d85ac91833aac1dbfa49e592cff
thisisyellow <73936439+thisisyellow@users.noreply.github.com>
2022-02-06 13:17:30 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+	private int count;
+
+	private float movementX;
+	private float movementY;
+
+	public float speed = 0;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+	
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winTextObject.SetActive(false);
+	}
+	void OnMove(InputValue movementValue)
+	{
+		Vector2 movementVector = movementValue.Get<Vector2>();
+		movementX = movementVector.x;
+		movementY = movementVector.y;
+	}
+	private void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		rb.AddForce(movement * speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+		void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12)
+		{
+			
+			winTextObject.SetActive(true);
+		}
+	}
+}
